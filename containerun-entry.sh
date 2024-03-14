23fd201b6b109bc251718c460f9a207dec7cd4f6
qiaoogan <qiaogang1984@163.com>
2024-03-14 11:50:34 +0800

    add exit code for jenkins run

--- containerun-entry.sh
+++ containerun-entry.sh
@@ -6,4 +6,14 @@ npm install
 
 export EXECUTE_ENV=container
 npm run test
+test_exit_code=$?
+
 npm run allure-generate
+last_exit_code=$?
+
+if [ $test_exit_code -ne 0 ] || [ $last_exit_code -ne 0 ]; then
+    echo "Test Failed"
+    exit 1
+else
+    echo "Test Completed"
+fi
